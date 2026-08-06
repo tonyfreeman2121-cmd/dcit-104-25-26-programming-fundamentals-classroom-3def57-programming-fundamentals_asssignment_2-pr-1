afb5606ec7b155875132551416385a1b734a7272
tonyfreeman2121-cmd <tonyfreeman2121@gmail.com>
2026-08-06 20:51:41 +0000

    Update assignment_01_prime_checker.cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,4 +36,30 @@
 
 #include <iostream>
 using namespace std;
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
+
 
